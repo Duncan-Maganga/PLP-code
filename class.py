97f6cf8cf0f036577af79eea06d6092f6f94118e
Duncan <duncanmaganga100@gmail.com>
2025-08-21 13:56:02 -0400

    Update class.py
    added code

--- class.py
+++ class.py
@@ -21,3 +21,13 @@ update_time()
 
 # Run the GUI
 root.mainloop()
+
+# Ask for first name
+first_name = input("Enter your first name: ")
+
+# Ask for second name
+second_name = input("Enter your second name: ")
+
+# Display full name
+print("Your full name is:", first_name, second_name)
+
